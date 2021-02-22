d50c3be243acf0e2dfc479c836fa742bfeff26f9
Helena Torres-Rocha <helena.torres-rocha@ocsbstudent.ca>
2021-02-22 15:20:30 +0000

    final copy

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ document.getElementById('button').addEventListener('click', newfunction)
 function newfunction () {
   day = document.getElementById('input').value
   age = document.getElementById('input-2').value
-  if ((day === 'Tuesday' || day === 'Thursday') || (age >= 12 && age <= 21)) { 
+  if ((day === 'Tuesday' || day === 'Thursday') || (age >= 12 && age <= 21)) {
     document.getElementById('paragraph').innerHTML = 'You are eligible for student pricing!'
   } else {
     document.getElementById('paragraph').innerHTML = 'you must pay regular price'
